c5a408d42a4d8457a71fe05245b3bd9ea1b60e91
DevBrunoFilipe <dev.bruno.filipe@gmail.com>
2017-09-13 13:19:11 +0200

    main page finished, minor adjustments needed registration offline warnings instated

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.listen(port, function(err)){
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+//Handles routing
+app.get('/', function(req, res){
+    //res.send('Main content.');
+    res.render('index', {
+        nav   : ['recruitment', 'resources', 'contact']
+    });
+});
+
+app.get('/register', function(req, res){
+    res.send("This website is currently under developemnt, as such, registrations are not yet available. Please return at a later date for our fully functioning website. We're sorry for the inconvenience and thank you for your patience.");
+    // res.render('index', {
+    //     nav   : ['recruitment', 'resources', 'contact']
+    // });
+});
+
+app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
-}
+})
