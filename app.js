68d545a16222cbb4f20e1d39c59a639fc9fcf4fe
DevBrunoFilipe <dev.bruno.filipe@gmail.com>
2017-10-04 18:00:51 +0200

    small fixes

--- app.js
+++ app.js
@@ -1,27 +1,68 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myapp', {
+    useMongoClient: true
+});
+//mongoose.connect('localhost:27017/kcgt');
+require ('./src/models/characters')
+
 var port = process.env.PORT || 3000;
+var authRouter = require('./src/routes/authRoutes');
+var forumRouter = require('./src/routes/forumRoutes');
+var userRouter = require('./src/routes/userRoutes');
+var testsRouter = require('./src/routes/testsRoutes');
+var charactersRouter = require('./src/routes/charactersRoutes');
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+
+app.use(function(req, res, next){
+    res.locals.paths = [
+        {Link: '../', Text: 'return'},
+        {Link: '../', Text: 'logout'}
+    ];
+    next();
+});
 
 //Handles routing
 app.get('/', function(req, res){
     //res.send('Main content.');
     res.render('index', {
-        nav   : ['recruitment', 'resources', 'contact']
+        nav: [
+            {Link: '#recruitment', Text: 'recruitment'},
+            {Link: '#resources', Text: 'resources'},
+            {Link: '#contacts', Text: 'contacts'},
+            {Link: '/auth', Text: 'login'}
+        ]
     });
 });
 
-app.get('/register', function(req, res){
-    res.send("This website is currently under developemnt, as such, registrations are not yet available. Please return at a later date for our fully functioning website. We're sorry for the inconvenience and thank you for your patience.");
-    // res.render('index', {
-    //     nav   : ['recruitment', 'resources', 'contact']
-    // });
-});
+
+//test running only
+app.use('/tests', testsRouter);
+//end test
+
+//user related
+    app.use('/auth', authRouter);
+//end user
+
+//forum related
+    app.use('/forum', forumRouter);
+    app.use('/forum/threads', forumRouter);
+//end forum
+
+//user related
+    app.use('/dashboard', userRouter);
+//end user
+
+//characters related
+    app.use('/dashboard/characters', charactersRouter);
+//end characters
 
 app.listen(port, function(err){
     console.log('The server is running on port: ' + port);
